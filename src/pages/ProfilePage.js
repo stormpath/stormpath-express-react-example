44289d54400f75e3d94096cab6151f327e317f1f
Robin Orheden <orhedenr@gmail.com>
2016-03-03 20:46:09 -0800

    add UserProfileForm support

--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -1,15 +1,9 @@
 import React from 'react';
 import DocumentTitle from 'react-document-title';
+import { UserProfileForm } from 'react-stormpath';
 
-import { UserComponent } from 'react-stormpath';
-
-export default class ProfilePage extends UserComponent {
-  onFormSubmit(e) {
-    e.preventDefault();
-  }
-  
+export default class ProfilePage extends React.Component {
   render() {
-    var user = this.state.user;
     return (
       <DocumentTitle title={`My Profile`}>
       <div className="container">
@@ -21,25 +15,7 @@ export default class ProfilePage extends UserComponent {
           </div>
           <div className="row">
             <div className="col-xs-12">
-              <form className="form-horizontal" submit={this.onFormSubmit.bind(this)}>
-                <div className="form-group">
-                  <label htlmFor="spFirstName" className="col-xs-12 col-sm-4 control-label">First Name</label>
-                  <div className="col-xs-12 col-sm-4">
-                    <input className="form-control" id="spFirstName" value={this.state.user.givenName} placeholder="First Name" disabled="true" />
-                  </div>
-                </div>
-                <div className="form-group">
-                  <label htlmFor="spLastName" className="col-xs-12 col-sm-4 control-label">Last Name</label>
-                  <div className="col-xs-12 col-sm-4">
-                    <input className="form-control" id="spLastName" value={this.state.user.surname} placeholder="Last Name" disabled="true" />
-                  </div>
-                </div>
-                <div className="form-group">
-                  <div className="col-sm-offset-4 col-sm-4">
-                    <button type="submit" className="btn btn-primary" disabled="true">Save (not supported yet)</button>
-                  </div>
-                </div>
-              </form>
+              <UserProfileForm />
             </div>
           </div>
         </div>
