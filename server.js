b39eabe075082c451a2ca622ef48d6bbbbeb4705
Robin <orhedenr@gmail.com>
2016-04-19 15:17:53 +0200

    add missing /me route in server.js

--- server.js
+++ server.js
@@ -25,6 +25,46 @@ app.use(stormpath.init(app, {
   }
 }));
 
+app.post('/me', bodyParser.json(), stormpath.loginRequired, function (req, res) {
+  function writeError(message) {
+    res.status(400);
+    res.json({ message: message, status: 400 });
+    res.end();
+  }
+  
+  function saveAccount () {
+    req.user.givenName = req.body.givenName;
+    req.user.surname = req.body.surname;
+    req.user.email = req.body.email;
+    
+    req.user.save(function (err) {
+      if (err) {
+        return writeError(err.userMessage || err.message);
+      }
+      res.end();
+    });
+  }
+
+  if (req.body.password) {
+    var application = req.app.get('stormpathApplication');
+    
+    application.authenticateAccount({
+      username: req.user.username,
+      password: req.body.existingPassword
+    }, function (err) {
+      if (err) {
+        return writeError('The existing password that you entered was incorrect.');
+      }
+      
+      req.user.password = req.body.password;
+      
+      saveAccount();
+    });
+  } else {
+    saveAccount();
+  }
+});
+
 app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build/index.html'));
 });
